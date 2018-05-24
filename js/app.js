73512ba777ba39d7bf63352c885074cfd4342a6f
Nasim <mdnasimulhaque@hotmail.com>
2018-05-25 05:29:17 +0530

    modifications to loading

--- js/app.js
+++ js/app.js
@@ -1,26 +1,12 @@
-/*
-author: Boostraptheme
-author URL: https://boostraptheme.com
-License: Creative Commons Attribution 4.0 Unported
-License URL: https://creativecommons.org/licenses/by/4.0/
-*/ 
-
-/* ====================================================
-                        ANIMATION
-   ================================================= */
-    $(function () {
-        // animate on scroll
-        new WOW().init();
+$(function () {
+    new WOW().init();
+});
+(function($) {
+    'use strict';
+    $(document).ready(function(){
+        $('.loader').fadeOut(2000);
     });
 
-/* ====================================================
-                        NAVIGATION
-   ================================================= */
-
-(function($) {
-    "use strict"; // Start of use strict
-
-    // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
@@ -33,19 +19,13 @@ License URL: https://creativecommons.org/licenses/by/4.0/
             }
         }
     });
-
-    // Closes responsive menu when a scroll trigger link is clicked
     $('.js-scroll-trigger').click(function() {
         $('.navbar-collapse').collapse('hide');
     });
-
-    // Activate scrollspy to add active class to navbar items on scroll
     $('body').scrollspy({
         target: '#mainNav',
         offset: 64
     });
-
-    // Collapse Navbar
     var navbarCollapse = function() {
         if ($("#mainNav").offset().top > 100) {
             $("#mainNav").addClass("navbar-shrink");
@@ -53,12 +33,8 @@ License URL: https://creativecommons.org/licenses/by/4.0/
             $("#mainNav").removeClass("navbar-shrink");
         }
     };
-    // Collapse now if page is not at top
     navbarCollapse();
-    // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
-
-    // Hide navbar when modals trigger
     $('.portfolio-modal').on('show.bs.modal', function(e) {
         $(".navbar").addClass("d-none");
     })
@@ -66,12 +42,7 @@ License URL: https://creativecommons.org/licenses/by/4.0/
         $(".navbar").removeClass("d-none");
     })
 
-})(jQuery); // End of use strict
-
-
-/*====================================================
-                    GALlERY
-====================================================*/
+})(jQuery);
 $(function() {
     $("#gallery-slider").owlCarousel({
         items: 3,
@@ -96,23 +67,14 @@ $(function() {
         }
     });
 });
-
-/* ====================================================
-                       BACK TO TOP
-=======================================================*/
-  (function() {
-
-      $(window).scroll(function() {
-
-          if ($(this).scrollTop() < 50) {
-              // hide nav
-              $("nav").removeClass("ss-top-nav");
-              $("#back-to-top").fadeOut();
-
-          } else {
-              // show nav
-              $("nav").addClass("ss-top-nav");
-              $("#back-to-top").fadeIn();
-          }
-      });
-  }); // End of use strict
+(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() < 50) {
+            $("nav").removeClass("ss-top-nav");
+            $("#back-to-top").fadeOut();
+        } else {
+            $("nav").addClass("ss-top-nav");
+            $("#back-to-top").fadeIn();
+        }
+    });
+});
